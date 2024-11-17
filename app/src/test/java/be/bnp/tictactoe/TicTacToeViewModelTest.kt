9cec1deb991ffaff57f63635312d0d53f862621b
“anonymous1450” <jonathantwite1995@gmail.com>
2024-11-17 17:40:18 +0100

    Red - When X plays then X is mark and it's O's turn

--- app/src/test/java/be/bnp/tictactoe/TicTacToeViewModelTest.kt
+++ app/src/test/java/be/bnp/tictactoe/TicTacToeViewModelTest.kt
@@ -20,4 +20,14 @@ class TicTacToeViewModelTest{
     fun when_gameStarts_then_XPlaysFirst() {
         assertEquals(Player.X, viewModel.currentPlayer)
     }
+
+    @Test
+    fun when_XPlays_then_XIsMarked_And_OPlays(){
+        viewModel.onBoardClick(0,0)
+        assertEquals(Player.O, viewModel.currentPlayer)
+        assertEquals(viewModel.board[0][0], Player.X)
+        viewModel.onBoardClick(0,1)
+        assertEquals(Player.X, viewModel.currentPlayer)
+        assertEquals(viewModel.board[0][1], Player.O)
+    }
 }
