0d0094b6153c17ffc254b429438d77de771dd7d6
“anonymous1450” <jonathantwite1995@gmail.com>
2024-11-18 20:00:29 +0100

    Red - Screen - when X plays then X is marked and O should play

--- app/src/androidTest/java/be/bnp/tictactoe/TicTacToeScreenTest.kt
+++ app/src/androidTest/java/be/bnp/tictactoe/TicTacToeScreenTest.kt
@@ -1,12 +1,16 @@
 package be.bnp.tictactoe
 
 import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import be.bnp.tictactoe.ui.TicTacToeScreen
 import be.bnp.tictactoe.viewmodel.TicTacToeViewModel
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,13 +22,17 @@ class TicTacToeScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun when_gameStarts_then_XPlaysFirst() {
+    @Before
+    fun setUp() {
         composeTestRule.setContent {
             TicTacToeScreen(
                 viewModel = TicTacToeViewModel()
             )
         }
+    }
+
+    @Test
+    fun when_gameStarts_then_XPlaysFirst() {
 
         // Ensure it's X's turn
         composeTestRule
@@ -36,4 +44,20 @@ class TicTacToeScreenTest {
             .onAllNodesWithText("-")
             .assertCountEquals(9)
     }
+
+    @Test
+    fun when_XPlays_then_XIsMarked_And_OShouldPlays() {
+        composeTestRule.onNodeWithTag("0, 0").performClick()
+
+        composeTestRule.onNodeWithTag("0, 0").assertTextEquals("X")
+        composeTestRule.onNodeWithText("Player O's turn").assertExists()
+
+        composeTestRule.onNodeWithTag("0, 1").performClick()
+
+        composeTestRule.onNodeWithTag("0, 1").assertTextEquals("O")
+        composeTestRule.onNodeWithText("Player X's turn").assertExists()
+    }
+
+
+
 }
