2c8db3631963fda32404501c166f062f879cb0bb
“anonymous1450” <jonathantwite1995@gmail.com>
2024-11-18 20:05:23 +0100

    Green - Screen - when X plays then X is marked and O should play

--- app/src/main/java/be/bnp/tictactoe/ui/TicTacToeScreen.kt
+++ app/src/main/java/be/bnp/tictactoe/ui/TicTacToeScreen.kt
@@ -3,10 +3,8 @@ package be.bnp.tictactoe.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -14,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import be.bnp.tictactoe.model.Player
@@ -50,7 +49,8 @@ fun TicTacToeScreen(
                         BoardButton(
                             modifier = Modifier.size(80.dp).padding(4.dp),
                             text = buttonText,
-                            onClick = { viewModel.onBoardClick(row, col) }
+                            onClick = { viewModel.onBoardClick(row, col) },
+                            tag = "$col, $row"
                         )
                     }
                 }
@@ -64,11 +64,12 @@ fun TicTacToeScreen(
 fun BoardButton(
     modifier: Modifier,
     text: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    tag: String
 ) {
     Button(
         onClick = onClick,
-        modifier = modifier
+        modifier = modifier.testTag(tag)
     ) {
         Text(
             text = text,
